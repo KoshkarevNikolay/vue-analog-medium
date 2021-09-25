5ec86182de5f2d330c8be7807cd292449743b16f
Koshkarev Nikolay <KoshkarevNV@gmail.com>
2021-09-26 01:42:56 +0300

    all stack

--- src/main.js
+++ src/main.js
@@ -2,11 +2,51 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from 'vuelidate'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faChevronLeft,
+  faChevronRight,
+  faCheck,
+  faExclamationCircle,
+  faEye,
+  faEyeSlash,
+  faSignLanguage,
+  faSignInAlt,
+} from '@fortawesome/free-solid-svg-icons'
+
+import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons'
+
+library.add(
+  faChevronLeft,
+  faChevronRight,
+  faCheck,
+  faExclamationCircle,
+  faEye,
+  faEyeSlash,
+  faSignLanguage, // хлопки sign-language
+  faSignInAlt, // войти sign-in-alt
+  faEdit, // Изменить far edit
+  faTrashAlt, // Удалить far trash-alt
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'font-awesome-icon',
+  defaultIconPack: 'fas',
+})
+
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
